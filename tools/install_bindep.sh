f58a4d280690bba34e5818a23df16c82999250da
Paul Belanger <pabelanger@redhat.com>
2016-12-01 18:26:05 -0500

    Initial support for bindep
    Turtles all the way down! Install required ansible dependencies,
    before we can actually use ansible.
    
    Change-Id: I543798d2ef6cf9b1e624a7628a2992e99f2fd231
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ tools/install_bindep.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ex
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+function is_fedora {
+    [ -f /usr/bin/yum ] && cat /etc/*release | grep -q -e "Fedora"
+}
+
+YUM=yum
+if is_fedora; then
+    YUM=dnf
+fi
+
+if apt-get -v > /dev/null 2>&1 ; then
+    sudo apt-get update
+    sudo apt-get --assume-yes install `bindep -b bindep.txt test`
+else
+    sudo $YUM install -y `bindep -b bindep.txt test`
+fi
