973b73d60dbb25a2600182af97d4fe0a910af5bd
Paul Belanger <pabelanger@redhat.com>
2017-01-21 17:37:55 -0500

    Support depends-on for nodepool / zuul / etc
    Change-Id: I0aea13cbbd6efa660c52d9b331b3031f6e656cca
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ tools/install_openstack.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ex
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+CLONEMAP=`mktemp`
+function cleanup {
+    rm -f $CLONEMAP
+}
+trap cleanup EXIT
+cat > $CLONEMAP << EOF
+clonemap:
+  - name: 'openstack(-infra)/(.*)'
+    dest: '/tmp/\2'
+EOF
+
+zuul-cloner -m $CLONEMAP \
+  --cache-dir /opt/git \
+  git://git.openstack.org \
+  openstack/diskimage-builder \
+  openstack-infra/nodepool \
+  openstack-infra/shade \
+  openstack-infra/zuul
