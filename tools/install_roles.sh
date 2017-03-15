a06930c67a9fa5cf8aea1fcf7e0dbfd887f468d3
Paul Belanger <pabelanger@redhat.com>
2017-03-15 15:48:38 -0400

    Remove ansible-role-jenkins-job-builder
    We no longer need to depend on JJB, so remove it.
    
    Change-Id: Ifdf3c472608d5daf7699d9f1eed00b12e0f0f8cb
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/install_roles.sh
+++ tools/install_roles.sh
@@ -28,7 +28,6 @@ zuul-cloner -m $CLONEMAP \
   --cache-dir /opt/git \
   git://git.openstack.org \
   openstack/ansible-role-diskimage-builder \
-  openstack/ansible-role-jenkins-job-builder \
   openstack/ansible-role-nodepool \
   openstack/ansible-role-shade \
   openstack/ansible-role-ssh \
